2575e9c2f3b42b81c1aecb049315bd437454e50c
Sinamandla <121508711+sinamandla1@users.noreply.github.com>
2023-02-04 12:55:46 +0200

    Create SaveTheWorldOrDoomUsAll.py
    New game!

---
+++ Python/Save the world game/SaveTheWorldOrDoomUsAll.py
@@ -0,0 +1,60 @@
+#Welcome message
+
+#Question 1
+answer1 = input("Greetings! you have no choice but to answer these questions or else!\n so which country was Nelson Mandela born?:")
+if answer1 == "South Africa":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 2
+answer1 = input("Now in what year was Terminator 2 released?:")
+if answer1 == "1991":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 3
+answer1 = input("Which country has the world's tallest building?:")
+if answer1 == "Saudi Arabia":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 4
+answer1 = input("Should the world end anyway cause this is too easy?:")
+if answer1 == "No":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 5
+answer1 = input("Whats the answer to {5squared *(5+4)} / 2?:")
+if answer1 == "115":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 6
+answer1 = input("Who was the 30th president of the United States?:")
+if answer1 == "Calvin Coolidge":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#Question 6
+answer1 = input("Can your answers truly save the world right now?:")
+if answer1 == "No":
+    print("Nice one, NEXT")
+else:
+    print("you have DOOMED US ALL!!")
+    exit()
+
+#END
+print("Well played, your honesty kind of stopped me from blowing everything up... for now :D")
